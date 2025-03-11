0ce4fdce3300c39ebe34102587f464414458a9f6
gretchen.feldkamp <gretchen.feldkamp@MU-C1-21.mullen.pvt.k12.co.us>
2025-03-11 13:16:00 -0600

    Added new method

--- SISTeamProject/src/MainMenu.java
+++ SISTeamProject/src/MainMenu.java
@@ -1,5 +1,13 @@
 
 public class MainMenu
 	{
-
+		public static void main (String args[])
+		{
+		q1();
+		}
+		public static String q1()
+		{
+			String openingQ= "Welcome";
+			return openingQ;
+		}
 	}

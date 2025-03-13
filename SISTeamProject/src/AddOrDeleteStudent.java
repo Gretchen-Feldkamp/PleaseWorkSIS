02327ab49581f355d819199362c5951e840d9bbf
gretchen.feldkamp <gretchen.feldkamp@MU-C1-21.mullen.pvt.k12.co.us>
2025-03-13 13:15:31 -0600

    Made student into an object, fixed the hardcode

--- SISTeamProject/src/AddOrDeleteStudent.java
+++ SISTeamProject/src/AddOrDeleteStudent.java
@@ -1,3 +1,4 @@
+import java.util.Arrays;
 import java.util.Scanner;
 
 
@@ -27,10 +28,41 @@ public class AddOrDeleteStudent
 			}
 		public static void op1()
 		{
-			System.out.println("Please list your student's full name:");
-			Scanner a1 = new Scanner(System.in);
-			String studeName=a1.nextLine();
-			System.out.println("First period class and letter grade:");
+			
+			
+			
+//			System.out.println("Please list your student's full name:");
+//			Scanner a1 = new Scanner(System.in);
+//			String a1a = a1.nextLine();
+//			String[] studeName=a1a.split(" ");
+//			//first period[1] is the first name
+//			//first period[2] is the last name
+//			System.out.println("First period class and letter grade:");
+//			Scanner a2=new Scanner(System.in);
+//			String clasUno=a2.nextLine();
+//			String[] firstPeriod=clasUno.split(" ");
+//			//first period[1] is the class name
+//			//first period[2] is the letter grade
+//			System.out.println("Second period class and letter grade:");
+//			Scanner a3=new Scanner(System.in);
+//			String clasDos = a3.nextLine();
+//			String[] secondPeriod=clasDos.split(" ");
+//			//second period[1] is the class name
+//			//second period[2] is the letter grade
+//			System.out.println("Third period class and letter grade:");
+//			Scanner a4=new Scanner(System.in);
+//			String clasTres=a4.nextLine();
+//			String[] ThirdPeriod=clasTres.split(" ");
+//			//third period[1] is the class name
+//			//third period[2] is the letter grade
+//			System.out.println(studeName[0]);
+//			System.out.println(studeName[1]);
+//			System.out.println(firstPeriod[0]);
+//			System.out.println(firstPeriod[1]);
+//			System.out.println(secondPeriod[0]);
+//			System.out.println(secondPeriod[1]);
+//			System.out.println(ThirdPeriod[0]);
+//			System.out.println(ThirdPeriod[1]);
 			
 		}
 		

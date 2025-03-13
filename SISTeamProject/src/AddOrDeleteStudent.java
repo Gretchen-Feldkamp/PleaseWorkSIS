d7bfc81509f0d2a27d2a7b4cf4613c1332e82a1b
gretchen.feldkamp <gretchen.feldkamp@MU-C1-21.mullen.pvt.k12.co.us>
2025-03-13 12:33:28 -0600

    Added onto the add or delete, added submenue

--- SISTeamProject/src/AddOrDeleteStudent.java
+++ SISTeamProject/src/AddOrDeleteStudent.java
@@ -1,10 +1,37 @@
+import java.util.Scanner;
 
 public class AddOrDeleteStudent
 	{
 
 		public static void studentMain()
 			{
-				System.out.println("Welcome, please list your student's full name, GPA, and classes.");
+				Scanner answer= new Scanner(System.in);
+				System.out.println("Welcome! Would you like to:");
+				System.out.println("1. Add a student");
+				System.out.println("2. Delete a student");
+				String answerA = answer.nextLine();
+				if(answerA.equals("1"))
+					{
+						op1();
+					}
+				else if(answerA.equals("2"))
+					{
+						op2();
+					}
+				else
+					{
+						System.out.println("Please select a valid option.");
+						studentMain();
+					}
 			}
+		public static void op1()
+		{
+			System.out.println("1");
+		}
+		
+		public static void op2()
+		{
+			System.out.println("2");
+		}
 
 	}

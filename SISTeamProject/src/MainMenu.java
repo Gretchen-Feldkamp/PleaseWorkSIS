6ae13e412146ea9a1d4960aa1bb08215fd80af6e
gretchen.feldkamp <gretchen.feldkamp@MU-C1-21.mullen.pvt.k12.co.us>
2025-03-13 12:12:22 -0600

    Started scanner m,ethpd

--- SISTeamProject/src/MainMenu.java
+++ SISTeamProject/src/MainMenu.java
@@ -1,4 +1,4 @@
-
+import java.util.Scanner;
 public class MainMenu
 	{
 		public static void main (String args[])
@@ -7,10 +7,16 @@ public class MainMenu
 		}
 		public static void q1()
 		{
+			Scanner answer= new Scanner(System.in);
 			System.out.println("Welcome! What would you like to do?");
 			System.out.println("1. Add or Delete a student");
 			System.out.println("2. Change student Grades/Schedule");
 			System.out.println("3. Sort students");
+			String answerS = answer.nextLine();
+			if(answerS.equals("1"))
+				{
+					
+				}
 
 		}
 	}

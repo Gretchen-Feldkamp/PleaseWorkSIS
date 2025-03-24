aa91e7c71da9f17cb1f522f4c9bff8de287b1027
gretchen.feldkamp <gretchen.feldkamp@MU-C1-21.mullen.pvt.k12.co.us>
2025-03-24 10:02:01 -0600

    Deleted hardcode

--- SISTeamProject/src/AddOrDeleteStudent.java
+++ SISTeamProject/src/AddOrDeleteStudent.java
@@ -32,46 +32,22 @@ public class AddOrDeleteStudent
 		{
 			try
 				{
-			Scanner myFile = new Scanner(new File("StudentList.txt"));
-			while(myFile.hasNext())
+//			Scanner myFile = new Scanner(new File("StudentList.txt"));
+//			ArrayList<String> studentList = new ArrayList<Student>();
+//			while(myFile.hasNext())
 				{
-				String sentence = myFile.nextLine();
+					
 				}
-			catch()
 			
-//			System.out.println("Please list your student's full name:");
-//			Scanner a1 = new Scanner(System.in);
-//			String a1a = a1.nextLine();
-//			String[] studeName=a1a.split(" ");
-//			//first period[1] is the first name
-//			//first period[2] is the last name
-//			System.out.println("First period class and letter grade:");
-//			Scanner a2=new Scanner(System.in);
-//			String clasUno=a2.nextLine();
-//			String[] firstPeriod=clasUno.split(" ");
-//			//first period[1] is the class name
-//			//first period[2] is the letter grade
-//			System.out.println("Second period class and letter grade:");
-//			Scanner a3=new Scanner(System.in);
-//			String clasDos = a3.nextLine();
-//			String[] secondPeriod=clasDos.split(" ");
-//			//second period[1] is the class name
-//			//second period[2] is the letter grade
-//			System.out.println("Third period class and letter grade:");
-//			Scanner a4=new Scanner(System.in);
-//			String clasTres=a4.nextLine();
-//			String[] ThirdPeriod=clasTres.split(" ");
-//			//third period[1] is the class name
-//			//third period[2] is the letter grade
-//			System.out.println(studeName[0]);
-//			System.out.println(studeName[1]);
-//			System.out.println(firstPeriod[0]);
-//			System.out.println(firstPeriod[1]);
-//			System.out.println(secondPeriod[0]);
-//			System.out.println(secondPeriod[1]);
-//			System.out.println(ThirdPeriod[0]);
-//			System.out.println(ThirdPeriod[1]);
-			
+				}
+			catch(NullPointerException x)
+				{
+				System.out.println("No more students.");
+				}
+			catch(FileNotFoundException y)
+				{
+				System.out.println("No file found.");
+				}
 		}
 		
 		public static void op2()
